6fa929fcbe7d297ff696a50b90d86b0cd9900cde
Yogesh Kumar <dev@tranktechies.com>
2024-09-19 18:18:57 +0530

    code

--- Journal/settings.py
+++ Journal/settings.py
@@ -81,13 +81,23 @@ AUTH_USER_MODEL = "App.MyUser"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {  
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': "postgres",
+       'USER': 'postgres',
+       'PASSWORD': 'Test@123',
+       'HOST': '43.205.195.1',
+       'PORT': "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
